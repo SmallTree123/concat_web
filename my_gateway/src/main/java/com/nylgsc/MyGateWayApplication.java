7826065c6e7bbcd1fd8fb93b99d6b8663ecce57a
suchong <1793401593@qq.com>
2023-11-25 14:00:01 +0800

    feature：xxx_jobx项目

---
+++ my_gateway/src/main/java/com/nylgsc/MyGateWayApplication.java
@@ -0,0 +1,11 @@
+package com.nylgsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyGateWayApplication.class, args);
+    }
+}
